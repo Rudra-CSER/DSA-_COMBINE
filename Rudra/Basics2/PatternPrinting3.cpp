221614968b7b40b0c79d809463abe696512a47de
Rudra-CSER <97387423+Rudra-CSER@users.noreply.github.com>
2025-05-20 07:49:46 +0000

    (Pattern Printing) No 3,4  parrtens are printed(video Explanation dew)

---
+++ Rudra/Basics2/PatternPrinting3.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printPattern(int N){
+
+ // This is the outer loop which will loop for the rows.
+
+    for(int i = 1 ; i <= N ; i++){
+
+ // This is the inner loop which loops for the columns
+       // no. of columns = row number for each line here.
+       // Here, we print numbers from 1 to the row number
+       // instead of stars in each row.
+
+        for(int j= 1; j <= i ; j++){
+      cout<< j << " ";
+        }
+    // As soon as numbers for each iteration are printed, we move to the
+        // next row and give a line break otherwise all numbers
+        // would get printed in 1 line.
+
+       cout<< endl;
+    }
+}
+
+int main() {
+
+    // Here, we have taken the value of N as 5.
+    // We can also take input from the user.
+    int N =5;
+    printPattern(N);
+    return 0;
+}
