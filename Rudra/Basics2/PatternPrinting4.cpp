515e2b40ab8bc57ae1c3d2d39b04bfdab81432b9
Rudra-CSER <97387423+Rudra-CSER@users.noreply.github.com>
2025-05-26 14:33:00 +0000

    (Pattern Printing) No 8 parrtens are printed

--- Rudra/Basics2/PatternPrinting4.cpp
+++ Rudra/Basics2/PatternPrinting4.cpp
@@ -74,11 +74,17 @@ void print7(int n){
 void print8(int n){
   for (int i=0; i < n; i++){
     // space
-   
+ for(int j = 0 ; j < i ; j++){
+     cout<< " ";
+ }
     // star
-    
+    for(int j = 0 ; j < n*2-(2*i+1) ; j++){
+     cout<< "*";
+ }
     // space
-  
+  for(int j=0 ; j< i ; j++){
+   cout << " "; 
+  }
 
   cout << endl;
   }
