50697333d8f8fec05e890641ca799ca0b946fc7b
Rudra-CSER <97387423+Rudra-CSER@users.noreply.github.com>
2025-06-03 05:41:51 +0000

    (Pattern Printing) No 11, 12 parrtens are printed

--- Rudra/Basics2/PatternPrinting5.cpp
+++ Rudra/Basics2/PatternPrinting5.cpp
@@ -1,25 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
- void print2(int n){
- for(int i =0;i<n;i++){
- for(int j= 0 ; i< n ; j++){} 
-     cout<<"* ";
-   }
-   cout<< endl;
-}
+//  void print2(int n){
+//  for(int i =0;i<n;i++){
+//  for(int j= 0 ; i< n ; j++){} 
+//      cout<<"* ";
+//    }
+//    cout<< endl;
+// }
 
-void print5(int n){
+// void print5(int n){
 
- for(int i =0;i<n;i++){
- for(int j= 0 ; i< n-i+1 ; j++){} 
-     cout<<"* ";
-   }
-   cout<< endl;
-}
+//  for(int i =0;i<n;i++){
+//  for(int j= 0 ; i< n-i+1 ; j++){} 
+//      cout<<"* ";
+//    }
+//    cout<< endl;
+// }
 void print10(int n){
-print2(n); 
-print5(n); 
+  for(int i =1 ; i< 2*n-1 ; i++){
+        int starts = i ;
+        if(i>n) starts = 2*n -i ;
+        for(int j=1 ; j<= starts ; j++){
+          cout << "*";
+        }
+         cout << endl;
+      } 
+}
+
+void print11(int n){
+   int start =1 ;
+   for(int i= 0 ; i< n ; i++){
+      if(i % 2 == 0)start = 1;
+      else start = 0 ;
+      for(int j= 0 ; j<=i ;j++){
+        cout << start;
+        start = 1 -start;
+      } 
+ cout<< endl;
+   } 
+
+
+}
+
+void print12(int n){
+  int space = 2*(n -1 );
+  for(int i =1 ; i<= n ; i++){
+    //num
+   for(int j=1 ; j<= i ; j++){
+  cout << j;
+   }
+
+    //space
+for(int j =1 ; j<= space; j++){
+  cout<<" ";
+}
+
+    //num
+    for(int j=i ; j>= 1 ; j--){
+  cout << j;
+   }
+   cout << endl;
+   space -=2;
+  }
 }
 
 
@@ -38,7 +81,7 @@ int main()
     int n;
     cout << "Enter The input";
     cin >> n;
-    print10(n);
+    print12(n);
   }
   return 0;
 }
