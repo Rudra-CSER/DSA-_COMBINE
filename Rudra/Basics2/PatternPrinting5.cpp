6f20edd997792ccddd6f7a444a020e62e50c8102
Rudra-CSER <97387423+Rudra-CSER@users.noreply.github.com>
2025-06-12 17:59:49 +0000

    (Pattern Printing) No 13 - No 17 parrtens are printed

--- Rudra/Basics2/PatternPrinting5.cpp
+++ Rudra/Basics2/PatternPrinting5.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 //  void print2(int n){
 //  for(int i =0;i<n;i++){
-//  for(int j= 0 ; i< n ; j++){} 
+//  for(int j= 0 ; i< n ; j++){}
 //      cout<<"* ";
 //    }
 //    cout<< endl;
@@ -12,77 +12,161 @@ using namespace std;
 // void print5(int n){
 
 //  for(int i =0;i<n;i++){
-//  for(int j= 0 ; i< n-i+1 ; j++){} 
+//  for(int j= 0 ; i< n-i+1 ; j++){}
 //      cout<<"* ";
 //    }
 //    cout<< endl;
 // }
-void print10(int n){
-  for(int i =1 ; i< 2*n-1 ; i++){
-        int starts = i ;
-        if(i>n) starts = 2*n -i ;
-        for(int j=1 ; j<= starts ; j++){
-          cout << "*";
-        }
-         cout << endl;
-      } 
-}
-
-void print11(int n){
-   int start =1 ;
-   for(int i= 0 ; i< n ; i++){
-      if(i % 2 == 0)start = 1;
-      else start = 0 ;
-      for(int j= 0 ; j<=i ;j++){
-        cout << start;
-        start = 1 -start;
-      } 
- cout<< endl;
-   } 
-
-
-}
-
-void print12(int n){
-  int space = 2*(n -1 );
-  for(int i =1 ; i<= n ; i++){
-    //num
-   for(int j=1 ; j<= i ; j++){
-  cout << j;
-   }
-
-    //space
-for(int j =1 ; j<= space; j++){
-  cout<<" ";
-}
-
-    //num
-    for(int j=i ; j>= 1 ; j--){
-  cout << j;
-   }
-   cout << endl;
-   space -=2;
+void print10(int n)
+{
+  for (int i = 1; i < 2 * n - 1; i++)
+  {
+    int starts = i;
+    if (i > n)
+      starts = 2 * n - i;
+    for (int j = 1; j <= starts; j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
   }
 }
-void print13(){ 
+
+void print11(int n)
+{
+  int start = 1;
+  for (int i = 0; i < n; i++)
+  {
+    if (i % 2 == 0)
+      start = 1;
+    else
+      start = 0;
+    for (int j = 0; j <= i; j++)
+    {
+      cout << start;
+      start = 1 - start;
+    }
+    cout << endl;
+  }
 }
 
+void print12(int n)
+{
+  int space = 2 * (n - 1);
+  for (int i = 1; i <= n; i++)
+  {
+    // num
+    for (int j = 1; j <= i; j++)
+    {
+      cout << j;
+    }
 
-void print14(){
-  int streak = 0 ;
+    // space
+    for (int j = 1; j <= space; j++)
+    {
+      cout << " ";
+    }
 
+    // num
+    for (int j = i; j >= 1; j--)
+    {
+      cout << j;
+    }
+    cout << endl;
+    space -= 2;
+  }
 }
-void print15(){
-  int streak = 0 ;
 
+void print13(int n)
+{
+  int num = 1;
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      cout << num << " ";
+      num++;
+    }
+    cout << endl;
+  }
 }
- //printing the step with no of 
- //test case + no of itteration
 
+void print14(int n)
+{
+  for (int i = 0; i <= n; i++)
+  {
 
+    for (char ch = 'A'; ch <= 'A' + i; ch++)
+    {
+      cout << ch << " ";
+    }
+    cout << endl;
+  }
+}
+
+void print15(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+
+    for (char ch = 'A'; ch < 'A' + (n - i - 1); ch++)
+    {
+      cout << ch << " ";
+    }
+    cout << endl;
+  }
+}
+// printing the step with no of
+// test case + no of itteration
+void print16(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    char ch = 'A' + i;
+    for (int j = 0; j <= i; j++)
+    {
+      char ch = 'A' + i;
+      cout << ch << " ";
+    }
+
+    cout << endl;
+  }
+}
+
+void print17(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+
+    for (int j = 0; j <= n - i - 1; j++)
+    {
+      cout << " ";
+    }
+
+     char ch = 'A' ;
+    // adding a char BRake point that it will no be incremented further
+    char BrakePoint = (2 * i + 1) / 2;
+
+    for (int j = 1; j <= 2 * i + 1; j++)
+    {
+
+      cout << ch << " ";
+      if (j <= BrakePoint)
+        ch++;
+      else
+        ch--;
+    }
+
+    for (int j = 0; j < n - i - 1; j++)
+    {
+      cout << " ";
+    }
+    cout << endl;
+  }
+}
 int main()
 {
-  //make sure to upload on git
+  // make sure to upload on git
   int t;
   cin >> t; // if we initialize i with 0 the Test case will run 0,1(2ti)
   for (int i = 1; i <= t; i++)
@@ -91,7 +175,14 @@ int main()
     int n;
     cout << "Enter The input";
     cin >> n;
-    print12(n);
+    print16(n);
+    // cout<<"" << endl;
+    // print15(n);
+    // cout<<"" << endl;
+    // print14(n);
+    // print13(n);
+    // print12(n);
+    print17(n);
   }
   return 0;
 }
